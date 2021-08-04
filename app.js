5459e26d1115fdc3a9ca9cd5a90fa1c977683071
Otharius <llepennec101@gmail.com>
2021-08-04 10:42:07 +0200

    OK1

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ app.use(express.urlencoded({extended : false}));
 
 //Routes
 app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
-
-//const data = require('./data/users.json');
-//console.log(data);
-//console.log(data.users);
+app.use('/sign',require('./routes/sign'));
 
 app.listen(3000);
