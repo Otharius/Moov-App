3c0b4efbbe2ad0e462603ae38ae3e6b601e7d750
Otharius <llepennec101@gmail.com>
2022-05-06 14:55:12 +0200

    less

--- less/gulpfile.js
+++ less/gulpfile.js
@@ -3,7 +3,7 @@ const less = require('gulp-less');
 
 gulp.task('css', function() {
     return gulp
-        .src('./src/main.less')
+        .src('./src/computer.less')
         .pipe(less())
-        .pipe(gulp.dest('.'))
+        .pipe(gulp.dest('../public/css'))
 })
